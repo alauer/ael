23865f4ccb87498565b22d7bd442ebb61aa9f8ff
Aaron Lauer <lauer.aaron@gmail.com>
2019-01-30 13:49:15 -0500

    azure expressroute creation

---
+++ terraform/2-vpc-testing/us-west-2-vpc/azure_expr.tf
@@ -0,0 +1,18 @@
+resource "azurerm_express_route_circuit" "ael-kb-exprt" {
+  name                  = "ael-expressRoute1"
+  resource_group_name   = "us-west2-dev"
+  location              = "westus2"
+  service_provider_name = "Equinix"
+  peering_location      = "Seattle"
+  bandwidth_in_mbps     = 50
+
+  sku {
+    tier   = "Standard"
+    family = "MeteredData"
+  }
+
+  tags {
+  Terraform = "true"
+  Owner       = "aaron.lauer"
+  }
+}
--- terraform/2-vpc-testing/us-west-2-vpc/outputs.tf
+++ terraform/2-vpc-testing/us-west-2-vpc/outputs.tf
@@ -50,3 +50,8 @@ output "ael-kb-dxg1" {
   description = "List of IDs of DXG"
   value = ["${aws_dx_gateway.ael-kb-test.id}"]
 }
+
+output "ael-kb-exprt Service Key" {
+  description = "Expressroute Service Key"
+  value = ["${azurerm_express_route_circuit.ael-kb-exprt.service_key}"]
+}
