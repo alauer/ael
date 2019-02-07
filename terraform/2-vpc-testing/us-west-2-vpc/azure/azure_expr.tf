e0f84bc99036635cb07d633bc973422b3377baad
Aaron Lauer <lauer.aaron@gmail.com>
2019-02-07 13:16:12 -0500

    basic Vnet working

--- terraform/2-vpc-testing/us-west-2-vpc/azure/azure_expr.tf
+++ terraform/2-vpc-testing/us-west-2-vpc/azure/azure_expr.tf
@@ -1,5 +1,5 @@
 variable "azure_resource_group_name" {
-  default = "ael-test-personal"
+  default = "us-east-sol-eng"
 }
 variable "azure_location" {
   default = "eastus"
@@ -8,9 +8,14 @@ variable "azure_peering_location"{
   type = "map"
   default = {
     "westus2" = "Seattle"
+    "eastus" = "Asburn"
+    "eastus2" = "Ashburn"
   }
 }
 
+variable "office_ip" {
+  default = "136.41.224.23/32"
+}
 
 
 
@@ -18,7 +23,56 @@ provider "azurerm" {
   version = "~> 1.21"
 }
 
-resource "azurerm_express_route_circuit" "ael-kb-exprt" {
+
+resource "azurerm_virtual_network" "test" {
+  name                = "virtualNetwork1"
+  location            = "${var.azure_location}"
+  resource_group_name = "${var.azure_resource_group_name}"
+  address_space       = ["10.100.0.0/16"]
+  dns_servers         = ["10.100.0.4", "10.100.0.5"]
+
+  tags {
+    Terraform = "true"
+    Owner = "aaron.lauer"
+  }
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "testsubnet"
+  resource_group_name  = "${var.azure_resource_group_name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.100.1.0/24"
+
+}
+
+
+
+resource "azurerm_network_security_group" "ssh" {
+  name                = "ssh"
+  location            = "${var.azure_location}"
+  resource_group_name = "${var.azure_resource_group_name}"
+
+  security_rule {
+    name                       = "allow_all_ael"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefixes    = "${list(var.office_ip)}"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "ssh" {
+  subnet_id                 = "${azurerm_subnet.test.id}"
+  network_security_group_id = "${azurerm_network_security_group.ssh.id}"
+}
+
+
+/*resource "azurerm_express_route_circuit" "ael-kb-exprt" {
   name                  = "ael-expressRoute1"
   resource_group_name   = "${var.azure_resource_group_name}"
   location              = "${var.azure_location}"           #This needs to be in a variables file
@@ -35,50 +89,4 @@ resource "azurerm_express_route_circuit" "ael-kb-exprt" {
     Terraform = "true"
     Owner     = "aaron.lauer"
   }
-}
-
-#module "network" {
-#  source              = "Azure/network/azurerm"
-#  resource_group_name = "${var.azure_resource_group_name}"
-#  location            = "${var.azure_location}"
-#  address_space       = "10.20.0.0/16"
-#  subnet_prefixes     = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
-#  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-#  tags = {
-#    Terraform = "true"
-#    Owner     = "aaron.lauer"
-#  }
-#}
-
-resource "azurerm_subnet" "subnet" {
-  name  = "subnet1"
-  address_prefix = "10.20.1.0/24"
-  resource_group_name = "${var.azure_resource_group_name}"
-  virtual_network_name = "ael-kb-test1"
-}
-
-resource "azurerm_network_security_group" "ssh" {
-  depends_on          = ["module.network"]
-  name                = "ssh"
-  location            = "${var.azure_location}"
-  resource_group_name = "${var.azure_resource_group_name}"
-
-  security_rule {
-    name                       = "allow_all_ael"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefixes    = "${list("${var.vpc1_cidr}","${var.office_ip}")}"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_subnet_network_security_group_association" "ssh" {
-  subnet_id                 = "${azurerm_subnet.subnet.id}"
-  network_security_group_id = "${azurerm_network_security_group.ssh.id}"
-}
+}*/
