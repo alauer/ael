0569de6f481df968bce98caae805c6a96920e913
Aaron Lauer <lauer.aaron@gmail.com>
2019-04-30 14:53:27 -0400

    vnet and compute

--- terraform/multi-cloud-demo/series2_video_3clouds+instances/azure/westus-infra/1-expr/main.tf
+++ terraform/multi-cloud-demo/series2_video_3clouds+instances/azure/westus-infra/1-expr/main.tf
@@ -14,6 +14,7 @@ locals {
   azure_location            = "westus"
   azure_peering_location    = "Silicon Valley"
   office_ip                 = "136.41.224.23/32"
+  expr_id                   = "/subscriptions/c0d488be-6472-4d1d-ada5-40914167eeb4/resourceGroups/us-east-sol-eng/providers/Microsoft.Network/expressRouteCircuits/ael-KBexpressRoute1"
 }
 
 provider "azurerm" {
@@ -31,36 +32,45 @@ resource "azurerm_express_route_circuit_peering" "wordpress-demo" {
   shared_key                    = "IDt25veH3cDpGJ9uvmCZG"
 }
 
-module "vnet" {
-  source              = "Azure/vnet/azurerm"
-  resource_group_name = "${local.azure_resource_group_name}"
+//
+// CREATE VNET AND SUBNETS
+//
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ael-wordpress-demo"
   location            = "${local.azure_location}"
-  vnet_name           = "ael-wordpress-demo"
-  address_space       = "172.16.0.0/16"
-  subnet_prefixes     = ["172.16.33.0/27"]
-  subnet_names        = ["wordpress1"]
+  resource_group_name = "${local.azure_resource_group_name}"
+  address_space       = ["172.16.0.0/16"]
   dns_servers         = ["10.20.101.5", "10.20.102.5"]
 
-  tags {
+  tags = {
     Terraform   = "true"
     Environment = "1-expr"
     Owner       = "aaron.lauer"
   }
 }
 
-//
-//
-//   CREAT VNET GATEWAY AND SUBNET
-//
-//
+resource "azurerm_subnet" "wordpress1" {
+  depends_on           = ["azurerm_virtual_network.vnet"]
+  name                 = "wordpress1"
+  resource_group_name  = "${local.azure_resource_group_name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "172.16.33.0/27"
+}
 
 resource "azurerm_subnet" "ael-wordpress-gwvnet" {
-  depends_on           = ["module.vnet"]
+  depends_on           = ["azurerm_virtual_network.vnet"]
   name                 = "GatewaySubnet"
   resource_group_name  = "${local.azure_resource_group_name}"
-  virtual_network_name = "${module.vnet.vnet_name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   address_prefix       = "172.16.33.224/27"
 }
+
+//
+//
+//   CREATE VNET GATEWAY AND IT's PUBLIC IP
+//
+//
 
 resource "azurerm_public_ip" "ael-wordpress" {
   name                = "ael-wordpress-pip-vnetgw"
@@ -69,13 +79,15 @@ resource "azurerm_public_ip" "ael-wordpress" {
 
   allocation_method = "Dynamic"
 
-  tags {
-    Terraform = "true"
-    Owner     = "aaron.lauer"
+  tags = {
+    Terraform   = "true"
+    Environment = "1-expr"
+    Owner       = "aaron.lauer"
   }
 }
 
-resource "azurerm_virtual_network_gateway" "ael-kb-test" {
+resource "azurerm_virtual_network_gateway" "ael-wordpress" {
+  depends_on          = ["azurerm_subnet.ael-wordpress-gwvnet"]
   name                = "ael-wordpress-vnet-gw1"
   resource_group_name = "${local.azure_resource_group_name}"
   location            = "${local.azure_location}"
@@ -99,9 +111,85 @@ resource "azurerm_virtual_network_gateway" "ael-kb-test" {
 }
 
 //
-//
-//
-//
+//  VNET GW CONNECTION TO EXPR
 //
 //
 
+resource "azurerm_virtual_network_gateway_connection" "ael-kb-test" {
+  name                = "ael-wordpress-vnet-gw1-conn"
+  resource_group_name = "${local.azure_resource_group_name}"
+  location            = "${local.azure_location}"
+
+  type                       = "ExpressRoute"
+  virtual_network_gateway_id = "${azurerm_virtual_network_gateway.ael-wordpress.id}"
+
+  express_route_circuit_id = "${local.expr_id}"
+}
+
+//
+//  PROVISION COMPUTE INSTANCE
+//
+resource "azurerm_public_ip" "vm-main" {
+  name                = "ael-wordpress-pip"
+  resource_group_name = "${local.azure_resource_group_name}"
+  location            = "${local.azure_location}"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "ael-wordpress1" {
+  name                = "ael-wordpress-nic"
+  resource_group_name = "${local.azure_resource_group_name}"
+  location            = "${local.azure_location}"
+
+  ip_configuration {
+    name                          = "ael-wordpress-configuration1"
+    subnet_id                     = "${azurerm_subnet.wordpress1.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.vm-main.id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name = "ael-wordpress-vm"
+
+  resource_group_name   = "${local.azure_resource_group_name}"
+  location              = "${local.azure_location}"
+  network_interface_ids = ["${azurerm_network_interface.ael-wordpress1.id}"]
+  vm_size               = "Standard_B1s"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "sales-demo-vm"
+    admin_username = "soleng"
+    admin_password = "7%dUZ4iLM)KtwUzV"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "1-expr"
+    Owner       = "aaron.lauer"
+  }
+}
